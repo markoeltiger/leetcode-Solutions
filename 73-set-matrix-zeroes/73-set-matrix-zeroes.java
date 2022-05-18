ae00234144aa16cb5135cb66e83f5768282e8a23
Mark Saif <43094629+markoeltiger@users.noreply.github.com>
2022-05-18 07:06:06 +0200

    Time: 24 ms (5.04%), Space: 54.8 MB (6.11%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,19 +1,29 @@
 class Solution {
-  public void setZeroes(int[][] matrix) {
-        if(matrix == null || matrix.length == 0) return;
-        int height = matrix.length, width = matrix[0].length;
-        boolean[][] zero = new boolean[height][width];
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++){
-                if(matrix[i][j] == 0) zero[i][j] = true;
-            }
-        }
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++){
-                if(zero[i][j]){
-                    for(int k = 0; k < height; k++) matrix[k][j] = 0;
-                    for(int k = 0; k < width; k++) matrix[i][k] = 0;
+  public void setZeroes(int[][] na) {
+       int high =0,width=0;
+ArrayList<Integer> indexes = new ArrayList<Integer>();
+        ArrayList<Integer> indexes2 = new ArrayList<Integer>();
+        for (int i = 0; i < na.length; i++) {
+            for (int j = 0; j < na[i].length; j++) {
+                if (na[i][j] == 0) {
+                 high=i ; width= j;
+                 indexes.add(high);
+                 indexes2.add(width);
                 }
+             }
+        }
+
+        for (int i = 0; i < na.length; i++) {
+
+            for (int j = 0; j < na[i].length; j++) {
+                for (int x = 0 ; x<indexes.size();x++){
+
+                    na[indexes.get(x)][j]=0;
+
+                    na[i][indexes2.get(x)]=0;
+                }
+
+
             }
         }
     }
