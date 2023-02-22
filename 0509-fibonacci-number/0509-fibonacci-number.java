8e847f879f7e94cea0999ade619cff984f751466
Mark Saif <43094629+markoeltiger@users.noreply.github.com>
2023-02-22 10:28:50 +0200

    Time: 194 ms (5.20%), Space: 148.3 MB (5.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,10 @@
 class Solution {
     public int fib(int n) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+        if(map.containsKey(n)) return map.get(n);
                 if(n==0) return 0;
         if(n <= 2) return 1 ;
-        return fib(n-1) + fib(n-2);
+        map.put( n,fib(n-1) + fib(n-2));
+        return map.get(n);
     }
 }
